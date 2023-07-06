06af3951925e8f81322c3d606622f856fe9f391e
i.marunchak <i.marunchak@cgpb.ru>
2023-07-06 15:54:40 +0300

    added cross origin

--- src/main/java/okkt/side/graphics/controller/statController.java
+++ src/main/java/okkt/side/graphics/controller/statController.java
@@ -4,16 +4,14 @@ import lombok.RequiredArgsConstructor;
 import okkt.side.graphics.model.OperationalDto;
 import okkt.side.graphics.model.RatingDto;
 import okkt.side.graphics.service.statService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/stat")
+@CrossOrigin
 public class statController {
 
     private final statService statService;
