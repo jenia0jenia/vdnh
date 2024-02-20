792c12a67d6f585fb6c28a9ee9edaa8fd81e5309
Евгений <cher@cgpb.ru>
2024-02-20 12:01:55 +0300

    last fixes

--- frontend/script.bat
+++ frontend/script.bat
@@ -1,17 +1,5 @@
 echo on
 
-call npm run build
-echo coping...
-copy README.md .\build
-copy x86_64-linux-musl-simple-http-server .\build
-copy x86_64-windows-msvc-simple-http-server.exe .\build
-echo Done!
-
-Echo zipping...
-
-"C:\Program Files\7-Zip\7z.exe" a -tzip "build.zip" "build\*"
-
-echo Done!
 
 echo adding to github...
 
--- frontend/src/components/Nota3/Alphatab.tsx
+++ frontend/src/components/Nota3/Alphatab.tsx
@@ -20,17 +20,20 @@ function AlphaTab({ ...props }) {
   const { source, title } = nota;
 
   let alphatabApi: AlphaTabApi;
-  
+
   useEffect(function () {
-    console.log('mount')
+    console.log('mount');
     alphatabApi = alphatab('/xml/' + source);
   }, []);
-  
-  useEffect( () => () => {
-    console.log('unmount')
-    console.log(alphatabApi)
-    alphatabApi && alphatabApi.destroy();
-  }, []);
+
+  useEffect(
+    () => () => {
+      console.log('unmount');
+      console.log(alphatabApi);
+      alphatabApi && alphatabApi.destroy();
+    },
+    []
+  );
 
   return (
     <div className={'w-full pt-8 px-8' + (loading ? ' loading' : '')}>
@@ -86,6 +89,9 @@ function AlphaTab({ ...props }) {
                 ></path>
               </svg>
             </a>
+
+            {/* speed */}
+
             <span className='at-player-progress'>0%</span>
             <div className='at-song-info'>
               <span className='at-song-title'></span> -
@@ -136,15 +142,15 @@ function AlphaTab({ ...props }) {
                 aria-hidden='true'
                 focusable='false'
                 data-prefix='fas'
-                data-icon='retweet'
-                className='svg-inline--fa fa-retweet'
+                data-icon='print'
+                className='svg-inline--fa fa-print'
                 role='img'
                 xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 640 512'
+                viewBox='0 0 512 512'
               >
                 <path
                   fill='currentColor'
-                  d='M614.2 334.8C610.5 325.8 601.7 319.1 592 319.1H544V176C544 131.9 508.1 96 464 96h-128c-17.67 0-32 14.31-32 32s14.33 32 32 32h128C472.8 160 480 167.2 480 176v143.1h-48c-9.703 0-18.45 5.844-22.17 14.82s-1.656 19.29 5.203 26.16l80 80.02C499.7 445.7 505.9 448 512 448s12.28-2.344 16.97-7.031l80-80.02C615.8 354.1 617.9 343.8 614.2 334.8zM304 352h-128C167.2 352 160 344.8 160 336V192h48c9.703 0 18.45-5.844 22.17-14.82s1.656-19.29-5.203-26.16l-80-80.02C140.3 66.34 134.1 64 128 64S115.7 66.34 111 71.03l-80 80.02C24.17 157.9 22.11 168.2 25.83 177.2S38.3 192 48 192H96V336C96 380.1 131.9 416 176 416h128c17.67 0 32-14.31 32-32S321.7 352 304 352z'
+                  d='M448 192H64C28.65 192 0 220.7 0 256v96c0 17.67 14.33 32 32 32h32v96c0 17.67 14.33 32 32 32h320c17.67 0 32-14.33 32-32v-96h32c17.67 0 32-14.33 32-32V256C512 220.7 483.3 192 448 192zM384 448H128v-96h256V448zM432 296c-13.25 0-24-10.75-24-24c0-13.27 10.75-24 24-24s24 10.73 24 24C456 285.3 445.3 296 432 296zM128 64h229.5L384 90.51V160h64V77.25c0-8.484-3.375-16.62-9.375-22.62l-45.25-45.25C387.4 3.375 379.2 0 370.8 0H96C78.34 0 64 14.33 64 32v128h64V64z'
                 ></path>
               </svg>
             </a>
