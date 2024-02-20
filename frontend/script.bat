26da0188efec4e11054cc36dbfaa2efbf97bebc3
Евгений <cher@cgpb.ru>
2024-02-20 13:14:31 +0300

    last fixes

--- frontend/script.bat
+++ frontend/script.bat
@@ -1,6 +1,19 @@
 echo on
 
+call npm run build
+echo coping...
+copy README.md .\build
+copy x86_64-linux-musl-simple-http-server .\build
+copy x86_64-windows-msvc-simple-http-server.exe .\build
+echo Done!
 
+Echo zipping...
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip "build.zip" "build\*"
+
+echo Done!
+
+echo adding to github...
 
 git add .
 git commit -m"last fixes"
